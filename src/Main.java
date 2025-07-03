44a6f7d4c961cd930d713e32b9dfa774122fc5c3
Roshan Patel <roshanpatel@macbookair.home>
2025-07-03 17:42:58 -0400

    Added Scalaton structure of the code

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Show login/signup menu
+        // Call AuthService - To Authenticate the user
+        //Based on the Role if
+        // If user -> show event list and booking
+        // If admin -> show event management options
+    }
+}
